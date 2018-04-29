fa51f0eb7e806b8ceab1b9402fac0b6f01a3bbe6
t <t@SG5>
2018-04-29 16:10:36 +0200

    PurchaseOrder API

---
+++ SmartGate.ElRwad.WebAPI/Areas/purchases/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,429 @@
+﻿using SmartGate.ElRwad.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
+{
+    public class PurchaseOrderController : ApiController
+    {
+        private elRwadEntities db = new elRwadEntities();
+
+        /// <summary>
+        /// get all purchase order
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetAllPurchaseOrder()
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Select(s => new
+                {
+                    id = s.Id,
+                    userId = s.UserId,
+                    branchId = s.BranchId,
+                    showingBranch = s.Showing_Branches.NameAr,
+                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
+                    supplierId = s.SupplierId,
+                    supplierName = s.Supplier.NameAr,
+                    supplierRepresentativeId = s.SupplierRepresentativeId,
+                    totalPrice = s.TotalPrice,
+                    receivingAddress = s.ReceivingAddress,
+                    addressRegionId = s.AddressRegionId,
+                    addressCityId = s.AddressCityId,
+                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString(),
+                    isApprovFinancialManag = s.IsApprovFinancialManag.ToString()
+
+                }).ToList();
+                return purchaseOrder;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+
+
+        /// <summary>
+        /// Get all purchase orders between two dates
+        /// </summary>
+        /// <param name="fromDate"></param>
+        /// <param name="toDate"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetAllPurchaseOrderByDateNotAccepted(DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == false).Select(s => new
+                {
+                    id = s.Id,
+                    userId = s.UserId,
+                    branchId = s.BranchId,
+                    showingBranch = s.Showing_Branches.NameAr,
+                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
+                    supplierId = s.SupplierId,
+                    supplierName = s.Supplier.NameAr,
+                    supplierRepresentativeId = s.SupplierRepresentativeId,
+                    totalPrice = s.TotalPrice,
+                    receivingAddress = s.ReceivingAddress,
+                    addressRegionId = s.AddressRegionId,
+                    addressCityId = s.AddressCityId,
+                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
+
+                }).ToList();
+                return purchaseOrder;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+        [HttpGet]
+        public dynamic GetAllPurchaseOrderByDateAccepted(DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == true).Select(s => new
+                {
+                    id = s.Id,
+                    userId = s.UserId,
+                    branchId = s.BranchId,
+                    showingBranch = s.Showing_Branches.NameAr,
+                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
+                    supplierId = s.SupplierId,
+                    supplierName = s.Supplier.NameAr,
+                    supplierRepresentativeId = s.SupplierRepresentativeId,
+                    totalPrice = s.TotalPrice,
+                    receivingAddress = s.ReceivingAddress,
+                    addressRegionId = s.AddressRegionId,
+                    addressCityId = s.AddressCityId,
+                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
+
+                }).ToList();
+
+                return purchaseOrder;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+        [HttpGet]
+        public dynamic GetAllPurchaseOrderByDateNotTakedActions(DateTime fromDate, DateTime toDate)
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == null).Select(s => new
+                {
+                    id = s.Id,
+                    userId = s.UserId,
+                    branchId = s.BranchId,
+                    showingBranch = s.Showing_Branches.NameAr,
+                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
+                    supplierId = s.SupplierId,
+                    supplierName = s.Supplier.NameAr,
+                    supplierRepresentativeId = s.SupplierRepresentativeId,
+                    totalPrice = s.TotalPrice,
+                    receivingAddress = s.ReceivingAddress,
+                    addressRegionId = s.AddressRegionId,
+                    addressCityId = s.AddressCityId,
+                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
+
+                }).ToList();
+                return purchaseOrder;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+        public dynamic GetAllPurchaseOrderNotTakedActions()
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Where(e => e.IsApprovFinancialManag == null).Select(s => new
+                {
+                    id = s.Id,
+                    userId = s.UserId,
+                    branchId = s.BranchId,
+                    showingBranch = s.Showing_Branches.NameAr,
+                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
+                    supplierId = s.SupplierId,
+                    supplierName = s.Supplier.NameAr,
+                    supplierRepresentativeId = s.SupplierRepresentativeId,
+                    totalPrice = s.TotalPrice,
+                    receivingAddress = s.ReceivingAddress,
+                    addressRegionId = s.AddressRegionId,
+                    addressCityId = s.AddressCityId,
+                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
+
+                }).ToList();
+                return purchaseOrder;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+        /// <summary>
+        /// get purchase order by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetByPurchaseOrederId(int id)
+        {
+            try
+            {
+                var purchaseOrder = db.purchaseOrders.Where(s => s.Id == id).FirstOrDefault();
+                if (purchaseOrder != null)
+                {
+                    return new
+                    {
+                        userId = purchaseOrder.UserId,
+                        branchId = purchaseOrder.BranchId,
+                        showingBranch = purchaseOrder.Showing_Branches.NameAr,
+                        supplierRepresentative = purchaseOrder.SuppliersRepresentative.NameAr,
+                        supplierId = purchaseOrder.SupplierId,
+                        supplierName = purchaseOrder.Supplier.NameAr,
+                        supplierRepresentativeId = purchaseOrder.SupplierRepresentativeId,
+                        totalPrice = purchaseOrder.TotalPrice,
+                        receivingAddress = purchaseOrder.ReceivingAddress,
+                        addressRegionId = purchaseOrder.AddressRegionId,
+                        addressCityId = purchaseOrder.AddressCityId,
+                        orderDate = purchaseOrder.OrderDate.Value.ToString("yyyy-MM-dd"),
+                        isApprovFinancialManag = purchaseOrder.IsApprovFinancialManag.ToString()
+                    };
+                }
+                else
+                {
+                    return new
+                    {
+                        Id = 0
+                    };
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="branchId"></param>
+        /// <param name="supplierId"></param>
+        /// <param name="supplierRepresentativeId"></param>
+        /// <param name="orderDate"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public dynamic PostPurchaseOrder(int userId, int branchId, int supplierId, int supplierRepresentativeId, DateTime orderDate)
+        {
+            var purchaseOrder = db.purchaseOrders.Add(new purchaseOrder
+            {
+                UserId = userId,
+                BranchId = branchId,
+                SupplierId = supplierId,
+                SupplierRepresentativeId = supplierRepresentativeId,
+                IsApprovFinancialManag = null,
+                OrderDate = orderDate
+
+            });
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                result = result,
+                id = purchaseOrder.Id
+
+            };
+        }
+
+
+
+        /// <summary>
+        /// to add purchase order's   total price & receiving Address & address Region & address City
+        /// </summary>
+        /// <param name="purchaseOrderId"></param>
+        /// <param name="totalPrice"></param>
+        /// <param name="receivingAddress"></param>
+        /// <param name="addressRegionId"></param>
+        /// <param name="addressCityId"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutPurchaseOrder(int id, float totalPrice, string receivingAddress, int addressRegionId, int addressCityId)
+        {
+            var purchaseOrder = db.purchaseOrders.Find(id);
+            purchaseOrder.TotalPrice = totalPrice;
+            purchaseOrder.ReceivingAddress = receivingAddress;
+            purchaseOrder.AddressRegionId = addressRegionId;
+            purchaseOrder.AddressCityId = addressCityId;
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result
+            };
+
+        }
+
+
+        /// <summary>
+        /// update all data in purchase order without total price
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="userId"></param>
+        /// <param name="branchId"></param>
+        /// <param name="supplierId"></param>
+        /// <param name="supplierRepresentativeId"></param>
+        /// <param name="receivingAddress"></param>
+        /// <param name="addressRegionId"></param>
+        /// <param name="addressCityId"></param>
+        /// <param name="orderDate"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutPurchaseOrder(int id, int userId, int branchId, int supplierId, int supplierRepresentativeId
+            , string receivingAddress, int addressRegionId, int addressCityId, DateTime orderDate)
+        {
+            var purchaseOrder = db.purchaseOrders.Find(id);
+            if (purchaseOrder.IsApprovFinancialManag == true)
+            {
+                return new
+                {
+                    result = "can't update in approved orders"
+                };
+            }
+
+            purchaseOrder.UserId = userId;
+            purchaseOrder.BranchId = branchId;
+            purchaseOrder.SupplierId = supplierId;
+            purchaseOrder.SupplierRepresentativeId = supplierRepresentativeId;
+            purchaseOrder.ReceivingAddress = receivingAddress;
+            purchaseOrder.AddressRegionId = addressRegionId;
+            purchaseOrder.AddressCityId = addressCityId;
+            purchaseOrder.OrderDate = orderDate;
+            purchaseOrder.IsApprovFinancialManag = null;
+
+
+
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result
+            };
+
+        }
+
+
+
+        /// <summary>
+        /// update total price
+        /// </summary>
+        /// <param name="purchaseOrderId"></param>
+        /// <param name="totalPrice"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutPurchaseOrder(int id, float totalPrice)
+        {
+            var purchaseOrder = db.purchaseOrders.Find(id);
+            purchaseOrder.TotalPrice = totalPrice;
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result
+            };
+
+        }
+
+        /// <summary>
+        /// approv by financial managment
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="isApprovFinancialManag"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutPurchaseOrder(int id, bool isApprovFinancialManag)
+        {
+            var purchaseOrder = db.purchaseOrders.Find(id);
+            purchaseOrder.IsApprovFinancialManag = isApprovFinancialManag;
+
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result
+            };
+
+        }
+
+        /// <summary>
+        /// delete purchase order
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AcceptVerbs("GET", "POST")]
+        [HttpDelete]
+        public dynamic DeletePurchaseOrder(int id)
+        {
+            var purchaseOrder = db.purchaseOrders.Where(s => s.Id == id).FirstOrDefault();
+            db.purchaseOrders.Remove(purchaseOrder);
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                result = result
+            };
+        }
+
+    }
+}
---
+++ SmartGate.ElRwad.WebAPI/Areas/purchases/Controllers/PurchaseOrderDetailsController.cs
@@ -0,0 +1,261 @@
+﻿using SmartGate.ElRwad.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
+{
+    public class PurchaseOrderDetailsController : ApiController
+    {
+        private elRwadEntities db = new elRwadEntities();
+
+
+
+        [HttpGet]
+        public dynamic GetAllDetailsPurchaseOrder()
+        {
+            try
+            {
+                var purchaseOrderDetails = db.PurchaseOrderDetails.Select(s => new
+                {
+                    id = s.Id,
+                    purchaseOrderId = s.PurchaseOrderId,
+                    brandId = s.BrandId,
+                    brandName = s.Brand.NameAr,
+                    modelId = s.ModelId,
+                    modelName = s.Model.NameAr,
+                    manufacturingYearId = s.ManufacturingYearId,
+                    manufacturingYear = s.ManufacturingYear.Year,
+                    categoryId = s.CategoryId,
+                    categoryName = s.CarsCategory.NameAr,
+                    colorId = s.ColorId,
+                    color = s.Color.NameAr,
+                    isNew = s.IsNew.ToString(),
+                    //isBuying = s.IsBuying.ToString(),
+                    count = s.Count,
+                    price = s.Price,
+                    //isReceived = s.IsReceived.ToString()
+
+                }).ToList();
+                return purchaseOrderDetails;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+
+        /// <summary>
+        /// get all details related to purchase order
+        /// </summary>
+        /// <param name="purchaseOrderId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetAllDetailsPurchaseOrderByPurchaseOrderId(int purchaseOrderId)
+        {
+            try
+            {
+                var purchaseOrderDetails = db.PurchaseOrderDetails.Where(e => e.PurchaseOrderId == purchaseOrderId).Select(s => new
+                {
+                    id = s.Id,
+                    purchaseOrderId = s.PurchaseOrderId,
+                    brandId = s.BrandId,
+                    brandName = s.Brand.NameAr,
+                    modelId = s.ModelId,
+                    modelName = s.Model.NameAr,
+                    manufacturingYearId = s.ManufacturingYearId,
+                    manufacturingYear = s.ManufacturingYear.Year,
+                    categoryId = s.CategoryId,
+                    categoryName = s.CarsCategory.NameAr,
+                    colorId = s.ColorId,
+                    color = s.Color.NameAr,
+                    isNew = s.IsNew.ToString(),
+                    //isBuying = s.IsBuying.ToString(),
+                    count = s.Count,
+                    price = s.Price,
+                    //isReceived = s.IsReceived.ToString()
+
+                }).ToList();
+                return purchaseOrderDetails;
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// get Purchase Order by id
+        /// </summary>
+        /// <param name="PurchaseOrderDetailId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public dynamic GetPurchaseOrderDetailById(int id)
+        {
+            try
+            {
+                var PurchaseOrderDetail = db.PurchaseOrderDetails.Where(e => e.Id == id).FirstOrDefault();
+                if (PurchaseOrderDetail != null)
+                {
+                    return new
+                    {
+
+                        id = PurchaseOrderDetail.Id,
+                        purchaseOrderId = PurchaseOrderDetail.PurchaseOrderId,
+                        brandId = PurchaseOrderDetail.BrandId,
+                        brandName = PurchaseOrderDetail.Brand.NameAr,
+                        modelId = PurchaseOrderDetail.ModelId,
+                        modelName = PurchaseOrderDetail.Model.NameAr,
+                        manufacturingYearId = PurchaseOrderDetail.ManufacturingYearId,
+                        manufacturingYear = PurchaseOrderDetail.ManufacturingYear.Year,
+                        categoryId = PurchaseOrderDetail.CategoryId,
+                        categoryName = PurchaseOrderDetail.CarsCategory.NameAr,
+                        colorId = PurchaseOrderDetail.ColorId,
+                        color = PurchaseOrderDetail.Color.NameAr,
+
+                        isNew = PurchaseOrderDetail.IsNew.ToString(),
+                        //isBuying = PurchaseOrderDetail.IsBuying.ToString(),
+                        count = PurchaseOrderDetail.Count,
+                        price = PurchaseOrderDetail.Price,
+                        //isReceived = PurchaseOrderDetail.IsReceived.ToString()
+
+                    };
+                }
+                else
+                {
+                    return new
+                    {
+                        Id = 0
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    result = new
+                    {
+                        Id = 0
+                    }
+                };
+            }
+        }
+
+
+
+        /// <summary>
+        /// add new detail for purchase order
+        /// </summary>
+        /// <param name="purchaseOrderId"></param>
+        /// <param name="brandId"></param>
+        /// <param name="modelId"></param>
+        /// <param name="manufacturingYearId"></param>
+        /// <param name="categoryId"></param>
+        /// <param name="colorId"></param>
+        /// <param name="isNew"></param>
+        /// <param name="isBuying"></param>
+        /// <param name="count"></param>
+        /// <param name="price"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public dynamic PostPurchaseOrderDetails(int purchaseOrderId, int brandId, int modelId, int manufacturingYearId,
+            int categoryId, int colorId, bool isNew, bool isBuying, int count, float price)
+        {
+            var purchaseOrderDetails = db.PurchaseOrderDetails.Add(new PurchaseOrderDetail
+            {
+                PurchaseOrderId = purchaseOrderId,
+                BrandId = brandId,
+                ModelId = modelId,
+                ManufacturingYearId = manufacturingYearId,
+                CategoryId = categoryId,
+                ColorId = colorId,
+                IsNew = isNew,
+                //IsBuying = isBuying,
+                Count = count,
+                Price = price
+            });
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                result = result,
+                //M.Hamed Add Price*Count to developr to sum total for all items
+                //from purchaseOrderDetailsPrice = purchaseOrderDetails.Price 
+                purchaseOrderDetailsPrice = purchaseOrderDetails.Price * purchaseOrderDetails.Count,
+                purchaseOrderDetailsId = purchaseOrderDetails.Id
+
+            };
+        }
+
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutPurchaseOrderDetails(int id, int purchaseOrderId, int brandId, int modelId, int manufacturingYearId,
+            int categoryId, int colorId, bool isNew, bool isBuying, int count, float price)
+        {
+            var purchaseOrderDetails = db.PurchaseOrderDetails.Find(id);
+            purchaseOrderDetails.PurchaseOrderId = purchaseOrderId;
+            purchaseOrderDetails.BrandId = brandId;
+            purchaseOrderDetails.ModelId = modelId;
+            purchaseOrderDetails.ManufacturingYearId = manufacturingYearId;
+            purchaseOrderDetails.CategoryId = categoryId;
+            purchaseOrderDetails.ColorId = colorId;
+            purchaseOrderDetails.IsNew = isNew;
+            //purchaseOrderDetails.IsBuying = isBuying;
+            purchaseOrderDetails.Count = count;
+            purchaseOrderDetails.Price = price;
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result,
+                purchaseOrderDetailsPrice = purchaseOrderDetails.Price * purchaseOrderDetails.Count
+            };
+
+        }
+
+        [HttpPut]
+        [AcceptVerbs("GET", "POST")]
+        public dynamic PutIsReceived(int id, bool isReceived)
+        {
+            var purchaseOrderDetails = db.PurchaseOrderDetails.Find(id);
+
+           // purchaseOrderDetails.IsReceived = isReceived;
+
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                reslt = result
+            };
+
+        }
+
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpDelete]
+        public dynamic DeletePurchaseOrderDetails(int id)
+        {
+            var purchaseOrderDetails = db.PurchaseOrderDetails.Where(s => s.Id == id).FirstOrDefault();
+            db.PurchaseOrderDetails.Remove(purchaseOrderDetails);
+            var result = db.SaveChanges() > 0 ? true : false;
+            return new
+            {
+                result = result
+            };
+        }
+
+    }
+}
