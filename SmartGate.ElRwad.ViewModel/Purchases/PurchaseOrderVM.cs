8757fd8ff8219349c29e19a529e3732dbac52fc7
emangamalsayed <emangamalsayed@gmail.com>
2018-05-08 17:24:10 +0200

    PurchaseOrder Done

---
+++ SmartGate.ElRwad.ViewModel/Purchases/PurchaseOrderVM.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartGate.ElRwad.ViewModel.Purchases
+{
+    public class PurchaseOrderDetailVM
+    {
+        public int? id { get; set; }
+        public int userId { get; set; }
+        public int branchId { get; set; }
+        public string showingBranch { get; set; }
+        public string supplierRepresentative { get; set; }
+        public int supplierId { get; set; }
+        public string supplierName { get; set; }
+        public int supplierRepresentativeId { get; set; }
+        public int totalPrice { get; set; }
+        public string receivingAddress { get; set; }
+        public int? addressRegionId { get; set; }
+        public int? addressCityId { get; set; }
+        public string orderDate { get; set; }
+        public bool isApprovFinancialManag { get; set; }
+
+    }
+
+    public class PurchaseOrderVM
+    {
+        //public int? id { get; set; }
+        public int userId { get; set; }
+        public int branchId { get; set; }
+        public int supplierId { get; set; }
+        public int supplierRepresentativeId { get; set; }
+        public DateTime orderDate { get; set; }
+        public bool isApprovFinancialManag { get; set; }
+
+    }
+    public class PutPurchaseOrderVM
+    {
+        public int? id { get; set; }
+        public int addressRegionId { get; set; }
+        public string receivingAddress { get; set; }
+        public int addressCityId { get; set; }
+        public int totalPrice { get; set; }
+
+    }
+
+    public class PurchaseVM
+    {
+        public int? id { get; set; }
+        public int userId { get; set; }
+        public int branchId { get; set; }
+        public int supplierId { get; set; }
+        public int supplierRepresentativeId { get; set; }
+        public string receivingAddress { get; set; }
+        public int? addressRegionId { get; set; }
+        public int? addressCityId { get; set; }
+        public string orderDate { get; set; }
+
+    }
+
+    public class PutTotalPriceVM
+    {
+        public int id { get; set; }
+        public int totalPrice { get; set; }
+
+    }
+
+    public class PutIsApprovFinancialManagVM
+    {
+        public int id { get; set; }
+        public bool isApprovFinancialManag { get; set; }
+
+    }
+}
--- SmartGate.ElRwad.WebAPI/Areas/purchases/Controllers/PurchaseOrderController.cs
+++ SmartGate.ElRwad.WebAPI/Areas/purchases/Controllers/PurchaseOrderController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using SmartGate.ElRwad.ViewModel;
 
 namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
 {
@@ -19,189 +20,32 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         [HttpGet]
         public dynamic GetAllPurchaseOrder()
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Select(s => new
-                {
-                    id = s.Id,
-                    userId = s.UserId,
-                    branchId = s.BranchId,
-                    showingBranch = s.Showing_Branches.NameAr,
-                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
-                    supplierId = s.SupplierId,
-                    supplierName = s.Supplier.NameAr,
-                    supplierRepresentativeId = s.SupplierRepresentativeId,
-                    totalPrice = s.TotalPrice,
-                    receivingAddress = s.ReceivingAddress,
-                    addressRegionId = s.AddressRegionId,
-                    addressCityId = s.AddressCityId,
-                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString(),
-                    isApprovFinancialManag = s.IsApprovFinancialManag.ToString()
-
-                }).ToList();
-                return purchaseOrder;
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetAllPurchaseOrder();
         }
 
 
 
-        /// <summary>
-        /// Get all purchase orders between two dates
-        /// </summary>
-        /// <param name="fromDate"></param>
-        /// <param name="toDate"></param>
-        /// <returns></returns>
+         
         [HttpGet]
         public dynamic GetAllPurchaseOrderByDateNotAccepted(DateTime fromDate, DateTime toDate)
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == false).Select(s => new
-                {
-                    id = s.Id,
-                    userId = s.UserId,
-                    branchId = s.BranchId,
-                    showingBranch = s.Showing_Branches.NameAr,
-                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
-                    supplierId = s.SupplierId,
-                    supplierName = s.Supplier.NameAr,
-                    supplierRepresentativeId = s.SupplierRepresentativeId,
-                    totalPrice = s.TotalPrice,
-                    receivingAddress = s.ReceivingAddress,
-                    addressRegionId = s.AddressRegionId,
-                    addressCityId = s.AddressCityId,
-                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
-
-                }).ToList();
-                return purchaseOrder;
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetAllPurchaseOrderByDateNotAccepted(fromDate, toDate);
         }
 
         [HttpGet]
         public dynamic GetAllPurchaseOrderByDateAccepted(DateTime fromDate, DateTime toDate)
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == true).Select(s => new
-                {
-                    id = s.Id,
-                    userId = s.UserId,
-                    branchId = s.BranchId,
-                    showingBranch = s.Showing_Branches.NameAr,
-                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
-                    supplierId = s.SupplierId,
-                    supplierName = s.Supplier.NameAr,
-                    supplierRepresentativeId = s.SupplierRepresentativeId,
-                    totalPrice = s.TotalPrice,
-                    receivingAddress = s.ReceivingAddress,
-                    addressRegionId = s.AddressRegionId,
-                    addressCityId = s.AddressCityId,
-                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
-
-                }).ToList();
-
-                return purchaseOrder;
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetAllPurchaseOrderByDateAccepted(fromDate, toDate);
         }
         [HttpGet]
         public dynamic GetAllPurchaseOrderByDateNotTakedActions(DateTime fromDate, DateTime toDate)
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Where(e => e.OrderDate >= fromDate.Date && e.OrderDate <= toDate.Date && e.IsApprovFinancialManag == null).Select(s => new
-                {
-                    id = s.Id,
-                    userId = s.UserId,
-                    branchId = s.BranchId,
-                    showingBranch = s.Showing_Branches.NameAr,
-                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
-                    supplierId = s.SupplierId,
-                    supplierName = s.Supplier.NameAr,
-                    supplierRepresentativeId = s.SupplierRepresentativeId,
-                    totalPrice = s.TotalPrice,
-                    receivingAddress = s.ReceivingAddress,
-                    addressRegionId = s.AddressRegionId,
-                    addressCityId = s.AddressCityId,
-                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
-
-                }).ToList();
-                return purchaseOrder;
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetAllPurchaseOrderByDateNotTakedActions(fromDate, toDate);
         }
 
         public dynamic GetAllPurchaseOrderNotTakedActions()
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Where(e => e.IsApprovFinancialManag == null).Select(s => new
-                {
-                    id = s.Id,
-                    userId = s.UserId,
-                    branchId = s.BranchId,
-                    showingBranch = s.Showing_Branches.NameAr,
-                    supplierRepresentative = s.SuppliersRepresentative.NameAr,
-                    supplierId = s.SupplierId,
-                    supplierName = s.Supplier.NameAr,
-                    supplierRepresentativeId = s.SupplierRepresentativeId,
-                    totalPrice = s.TotalPrice,
-                    receivingAddress = s.ReceivingAddress,
-                    addressRegionId = s.AddressRegionId,
-                    addressCityId = s.AddressCityId,
-                    orderDate = s.OrderDate.Value.Year.ToString() + "-" + s.OrderDate.Value.Month.ToString() + "-" + s.OrderDate.Value.Day.ToString()
-
-                }).ToList();
-                return purchaseOrder;
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetAllPurchaseOrderNotTakedActions();
         }
         /// <summary>
         /// get purchase order by id
@@ -211,51 +55,11 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         [HttpGet]
         public dynamic GetByPurchaseOrederId(int id)
         {
-            try
-            {
-                var purchaseOrder = db.purchaseOrders.Where(s => s.Id == id).FirstOrDefault();
-                if (purchaseOrder != null)
-                {
-                    return new
-                    {
-                        userId = purchaseOrder.UserId,
-                        branchId = purchaseOrder.BranchId,
-                        showingBranch = purchaseOrder.Showing_Branches.NameAr,
-                        supplierRepresentative = purchaseOrder.SuppliersRepresentative.NameAr,
-                        supplierId = purchaseOrder.SupplierId,
-                        supplierName = purchaseOrder.Supplier.NameAr,
-                        supplierRepresentativeId = purchaseOrder.SupplierRepresentativeId,
-                        totalPrice = purchaseOrder.TotalPrice,
-                        receivingAddress = purchaseOrder.ReceivingAddress,
-                        addressRegionId = purchaseOrder.AddressRegionId,
-                        addressCityId = purchaseOrder.AddressCityId,
-                        orderDate = purchaseOrder.OrderDate.Value.ToString("yyyy-MM-dd"),
-                        isApprovFinancialManag = purchaseOrder.IsApprovFinancialManag.ToString()
-                    };
-                }
-                else
-                {
-                    return new
-                    {
-                        Id = 0
-                    };
-
-                }
-            }
-            catch (Exception ex)
-            {
-                return new
-                {
-                    result = new
-                    {
-                        Id = 0
-                    }
-                };
-            }
+            return PurchaseOrderManager.Instance.GetByPurchaseOrederId(id);
         }
 
 
-
+        
 
         /// <summary>
         /// 
@@ -267,25 +71,9 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         /// <param name="orderDate"></param>
         /// <returns></returns>
         [HttpPost]
-        public dynamic PostPurchaseOrder(int userId, int branchId, int supplierId, int supplierRepresentativeId, DateTime orderDate)
+        public dynamic PostPurchaseOrder(PurchaseOrderVM p)
         {
-            var purchaseOrder = db.purchaseOrders.Add(new purchaseOrder
-            {
-                UserId = userId,
-                BranchId = branchId,
-                SupplierId = supplierId,
-                SupplierRepresentativeId = supplierRepresentativeId,
-                IsApprovFinancialManag = null,
-                OrderDate = orderDate
-
-            });
-            var result = db.SaveChanges() > 0 ? true : false;
-            return new
-            {
-                result = result,
-                id = purchaseOrder.Id
-
-            };
+           return PurchaseOrderManager.Instance.PostPurchaseOrder(p);
         }
 
 
@@ -301,18 +89,9 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         /// <returns></returns>
         [HttpPut]
         [AcceptVerbs("GET", "POST")]
-        public dynamic PutPurchaseOrder(int id, float totalPrice, string receivingAddress, int addressRegionId, int addressCityId)
+        public dynamic PutPurchaseOrder(PutPurchaseOrderVM p)
         {
-            var purchaseOrder = db.purchaseOrders.Find(id);
-            purchaseOrder.TotalPrice = totalPrice;
-            purchaseOrder.ReceivingAddress = receivingAddress;
-            purchaseOrder.AddressRegionId = addressRegionId;
-            purchaseOrder.AddressCityId = addressCityId;
-            var result = db.SaveChanges() > 0 ? true : false;
-            return new
-            {
-                reslt = result
-            };
+            return PurchaseOrderManager.Instance.PutPurchaseOrder(p);
 
         }
 
@@ -332,35 +111,9 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         /// <returns></returns>
         [HttpPut]
         [AcceptVerbs("GET", "POST")]
-        public dynamic PutPurchaseOrder(int id, int userId, int branchId, int supplierId, int supplierRepresentativeId
-            , string receivingAddress, int addressRegionId, int addressCityId, DateTime orderDate)
+        public dynamic PutPurchaseOrder(PurchaseVM p)
         {
-            var purchaseOrder = db.purchaseOrders.Find(id);
-            if (purchaseOrder.IsApprovFinancialManag == true)
-            {
-                return new
-                {
-                    result = "can't update in approved orders"
-                };
-            }
-
-            purchaseOrder.UserId = userId;
-            purchaseOrder.BranchId = branchId;
-            purchaseOrder.SupplierId = supplierId;
-            purchaseOrder.SupplierRepresentativeId = supplierRepresentativeId;
-            purchaseOrder.ReceivingAddress = receivingAddress;
-            purchaseOrder.AddressRegionId = addressRegionId;
-            purchaseOrder.AddressCityId = addressCityId;
-            purchaseOrder.OrderDate = orderDate;
-            purchaseOrder.IsApprovFinancialManag = null;
-
-
-
-            var result = db.SaveChanges() > 0 ? true : false;
-            return new
-            {
-                reslt = result
-            };
+            return PurchaseOrderManager.Instance.PutPurchaseOrder(p);
 
         }
 
@@ -374,15 +127,9 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         /// <returns></returns>
         [HttpPut]
         [AcceptVerbs("GET", "POST")]
-        public dynamic PutPurchaseOrder(int id, float totalPrice)
+        public dynamic PutPurchaseOrder(PutIsApprovFinancialManagVM p)
         {
-            var purchaseOrder = db.purchaseOrders.Find(id);
-            purchaseOrder.TotalPrice = totalPrice;
-            var result = db.SaveChanges() > 0 ? true : false;
-            return new
-            {
-                reslt = result
-            };
+            return PurchaseOrderManager.Instance.PutPurchaseOrder(p);
 
         }
 
@@ -396,15 +143,7 @@ namespace SmartGate.ElRwad.WebAPI.Areas.purchases.Controllers
         [AcceptVerbs("GET", "POST")]
         public dynamic PutPurchaseOrder(int id, bool isApprovFinancialManag)
         {
-            var purchaseOrder = db.purchaseOrders.Find(id);
-            purchaseOrder.IsApprovFinancialManag = isApprovFinancialManag;
-
-            var result = db.SaveChanges() > 0 ? true : false;
-            return new
-            {
-                reslt = result
-            };
-
+            return PurchaseOrderManager.Instance.PutPurchaseOrder(p);
         }
 
         /// <summary>
